33600d0e3843df4925d00c1e78199b14e940e3f4
mygit <purceladzedavit@gmail.com>
2023-01-20 18:37:22 +0400

    props and custom events

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <header>
+      <h1>Active Users</h1>
+    </header>
+    <user-data @add-user="addUser"></user-data>
+    <ul>
+      <active-user
+        v-for="user in users"
+        :key="user.id"
+        :name="user.name"
+        :age="user.age"
+      ></active-user>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: "dato",
+          name: "dato purceladze",
+          age: 24,
+        },
+        {
+          id: "ana",
+          name: "ana khundadze",
+          age: 23,
+        },
+      ],
+    };
+  },
+  methods: {
+    addUser(name, age) {
+      const newUser = {
+        id: new Date().toISOString(),
+        name: name,
+        age: age,
+      };
+      this.users.push(newUser);
+    },
+  },
+};
+</script>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Username: {{ name }}</h2>
+  <h3>Age: {{ age }}</h3>
+</template>
+
+<script>
+export default {
+  props: ["name", "age"],
+  data() {},
+};
+</script>
